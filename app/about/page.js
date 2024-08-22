220619cc7319cbd8a69eb06cdeff5a6159dd979d
Ahmed Mohamed Ali <71728993+ahmedhakk@users.noreply.github.com>
2024-08-22 16:22:08 +0300

    first commit

---
+++ app/about/page.js
@@ -0,0 +1,11 @@
+import Navigation from "../components/Navigation";
+
+const Page = () => {
+  return (
+    <div>
+      <h1>About page</h1>
+    </div>
+  );
+};
+
+export default Page;
---
+++ app/account/page.js
@@ -0,0 +1,11 @@
+import Navigation from "../components/Navigation";
+
+const Page = () => {
+  return (
+    <div>
+      <h1>Account page</h1>
+    </div>
+  );
+};
+
+export default Page;
