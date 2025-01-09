d0ebbff40aed0c8e5ca0ee093729f13295a178f0
Ahmed Mohamed Ali <71728993+ahmedhakk@users.noreply.github.com>
2025-01-09 21:41:47 +0200

    feat: finish part 33

--- app/account/page.js
+++ app/account/page.js
@@ -4,9 +4,9 @@ export const metadata = {
 
 const Page = () => {
   return (
-    <div>
-      <h1>Account page</h1>
-    </div>
+    <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+      Welcome Ahmed
+    </h2>
   );
 };
 
